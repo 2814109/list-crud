cd034db661bc16b5f5cce2337c518234d781310d
2814109 <2814109k@gmail.com>
2024-01-27 14:05:44 +0900

    u

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+
+import "./App.css";
+
 const invoices = [
   {
     invoice: "INV001",
@@ -43,34 +63,56 @@ const invoices = [
   },
 ];
 
-export function TableDemo() {
+function App() {
   return (
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {invoices.map((invoice) => (
-          <TableRow key={invoice.invoice}>
-            <TableCell className="font-medium">{invoice.invoice}</TableCell>
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+    <>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead>Choice</TableHead>
+
+            <TableHead className="text-right">Amount</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">$2,500.00</TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <Row {...invoice} />
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 }
+
+const Row = (invoice: (typeof invoices)[0]) => {
+  return (
+    <TableRow key={invoice.invoice}>
+      <TableCell className="font-medium">{invoice.invoice}</TableCell>
+      <TableCell>{invoice.paymentStatus}</TableCell>
+      <TableCell>{invoice.paymentMethod}</TableCell>
+      <TableCell className="z-10">
+        <Selector />
+      </TableCell>
+      <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+    </TableRow>
+  );
+};
+
+const Selector = () => (
+  <Select>
+    <SelectTrigger className="w-[180px]">
+      <SelectValue placeholder="Theme" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectItem value="light">Light</SelectItem>
+      <SelectItem value="dark">Dark</SelectItem>
+      <SelectItem value="system">System</SelectItem>
+    </SelectContent>
+  </Select>
+);
+
+export default App;
